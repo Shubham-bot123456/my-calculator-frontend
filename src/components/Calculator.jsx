f6d6492664988f964875cd6a3873586fdc1be851
Shubham Jadhav <sjadhav11999@gmail.com>
2022-12-27 10:50:45 +0530

    new ui

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,7 +4,17 @@ class Calculator extends Component {
   componentDidMount() {
     this.setState({ inputstring: "" });
   }
-  state = { inputstring: "" };
+  state = {
+    inputstring: "",
+    mystyle: {
+      margin: "2px",
+      paddingTop: "20px",
+      paddingBottom: "20px",
+      paddingLeft: "26px",
+      paddingRight: "26px",
+      fontFamily: "Arial",
+    },
+  };
   setButtonvalue1 = () => {
     this.setState({ inputstring: this.state.inputstring + 1 });
   };
@@ -33,44 +43,187 @@ class Calculator extends Component {
   };
   render() {
     return (
-      <div>
+      <div
+        style={{ marginLeft: "500px", marginTop: "50px", marginRight: "500px" }}
+      >
         <input
           onChange={(e) => {
             this.setState({ inputstring: e.target.value });
           }}
           value={this.state.inputstring}
+          type="text"
+          class="form-control"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
         ></input>
         <div>
-          <button onClick={() => this.setButtonvalue(1)}>1</button>
-          <button onClick={() => this.setButtonvalue(2)}>2</button>
-          <button onClick={() => this.setButtonvalue(3)}>3</button>
+          <button
+            type="button"
+            class="btn btn-danger"
+            style={{
+              marginTop: "10px",
+              marginBottom: "5px",
+              paddingLeft: "120px",
+              paddingRight: "120px",
+              paddingTop: "20px",
+              paddingBottom: "20px",
+            }}
+            onClick={() => this.setButtonvalue("")}
+          >
+            Clear
+          </button>
         </div>
         <div>
-          <button onClick={() => this.setButtonvalue(4)}>4</button>
-          <button onClick={() => this.setButtonvalue(5)}>5</button>
-          <button onClick={() => this.setButtonvalue(6)}>6</button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(1)}
+          >
+            1
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(2)}
+          >
+            2
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(3)}
+          >
+            3
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue("+")}
+          >
+            +
+          </button>
         </div>
         <div>
-          <button onClick={() => this.setButtonvalue(7)}>7</button>
-          <button onClick={() => this.setButtonvalue(8)}>8</button>
-          <button onClick={() => this.setButtonvalue(9)}>9</button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(4)}
+          >
+            4
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(5)}
+          >
+            5
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(6)}
+          >
+            6
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue("-")}
+          >
+            -
+          </button>
         </div>
         <div>
-          <button onClick={() => this.setButtonvalue(0)}>0</button>
-          <button onClick={() => this.setButtonvalue("+")}>+</button>
-          <button onClick={() => this.getAnswer()}>=</button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(7)}
+          >
+            7
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(8)}
+          >
+            8
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(9)}
+          >
+            9
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue("*")}
+          >
+            x
+          </button>
         </div>
         <div>
-          <button onClick={() => this.setButtonvalue("*")}>x</button>
-          <button onClick={() => this.setButtonvalue("/")}>/`</button>
-          <button onClick={() => this.setButtonvalue("-")}>-</button>
-        </div>
-        <div>
-          <button onClick={() => this.setButtonvalue("(")}>(</button>
-          <button onClick={() => this.setButtonvalue(")")}>)</button>
-        </div>
-        <div>
-          <button onClick={() => this.setButtonvalue("")}>Clear</button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(0)}
+          >
+            0
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue("(")}
+          >
+            (
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue(")")}
+          >
+            )
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            style={this.state.mystyle}
+            onClick={() => this.setButtonvalue("/")}
+          >
+            /
+          </button>
+
+          <button
+            type="button"
+            class="btn btn-success"
+            style={{
+              marginTop: "10px",
+              marginBottom: "5px",
+              paddingLeft: "120px",
+              paddingRight: "120px",
+              paddingTop: "20px",
+              paddingBottom: "20px",
+            }}
+            onClick={() => this.getAnswer()}
+          >
+            =
+          </button>
         </div>
       </div>
     );
